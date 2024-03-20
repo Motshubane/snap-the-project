1170af36ca9078c73bc6ae4a130354a4bda20f68
Victor Morodi <victor.morodi@wundermanthompson.com>
2024-03-20 06:42:53 +0200

    snap-the-project: code updates.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,45 @@
-import React from 'react'
+import NavMenu from "./NavMenu.jsx";
+import { useState } from "react";
+import Sidebar from "./Sidebar.jsx";
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
+
   return (
-   <header>
-    <div className="flex py-5 px-3">
-      <div className="flex items-center pt-3">
-        <img src="images/logo.svg" alt="Snap logo" />
-      </div>
-      <div className="flex-grow flex items-center z-20">
-        <Navigation/>
-      </div>
-      <div className="flex items-center">
-        <a href="#">
-          <img src="images/icon-menu.svg" alt="menu icon" className="block lg:hidden" />
-        </a>
-        <div className="hidden lg:flex lg:space-x-10 lg:items-center">
-          <div className="hover:text-almost-black cursor-pointer">login</div>
-          <div>
-              <div><button className="border-2 hover:border-almost-black hover:text:almost-black rounded-xl px-4 py-2">Register</button></div>
+    <header className="container">
+      <div className="flex py-5 px-3">
+        <div className="flex items-center pt-3">
+          <img src="images/logo.svg" alt="Snap Logo" />
+        </div>
+        <div className="flex-grow flex items-center z-20">
+          <NavMenu />
+        </div>
+        <div className="flex items-center">
+          <button
+            onClick={() => setOpen(true)}
+            aria-expanded={open}
+            aria-controls="sidebar"
+            aria-label="Open Menu"
+          >
+            <img
+              className="block lg:hidden"
+              src="images/icon-menu.svg"
+              alt="Menu"
+            />
+          </button>
+          <Sidebar open={open} setOpen={setOpen} />
+          <div className="hidden lg:flex lg:space-x-10 lg:items-center">
+            <div className="hover:text-almost-black cursor-pointer">Login</div>
+            <div>
+              <button className="border-2 hover:border-almost-black hover:text-almost-black rounded-xl px-4 py-2">
+                Register
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-   </header>
+    </header>
   );
 };
 
-export default Header
+export default Header;
